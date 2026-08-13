37ea7edbe04f43b44e5ca9fda3f40e531d2dc824
John Lloyd Saban <64182024@chcc.edu.ph>
2026-08-13 15:35:41 +0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/saban-module7-vue-system/',
   plugins: [vue(), tailwindcss()],
 })
